34c6a3c43b1ad53614ff22b0cec20093463d38a1
aditya136 <adityabanerjee102@gmail.com>
2022-10-14 10:32:58 -0700

    Quiz game in python

--- test.py
+++ test.py
@@ -1,17 +1,45 @@
 import pygame
-import os
-pygame.font.init()
-pygame.mixer.init()
-WIDTH, HEIGHT = 1920, 1080
+from pygame import mixer
+
+
+WIDTH, HEIGHT = 900, 600
+
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Quiz game made by @aditya1366')
 
-WIDTH = (255, 255, 255)
-HEIGHT = (0, 0, 0)
-RED = (255, 255,0)
-YELLOW = (255, 255, 0)
+pygame.display.set_caption("Quiz game made by @aditya136")
 
-# BORDER = pygame.Rect(WIDTH//2 - 5,0, 10, HEIGHT)
+
+
+icon = pygame.image.load('ideas.png')
+
+pygame.display.set_icon(icon)
+
+WIN.fill((0, 0, 0))
+
+def draw_window():
+    pygame.display.update()
+
+FPS = 60
+
+
+
+
+
+def main():
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+         
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        draw_window()
+
+       
+       
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
